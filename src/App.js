e48694bc13bc14cbc1c683f13c546cef8f972c8c
Abdullah Abdul Gaffar Patni <abdullah.patni@skill-mine.com>
2023-06-24 17:00:26 +0530

    First commit

--- src/App.js
+++ src/App.js
@@ -1,23 +1,40 @@
-import logo from './logo.svg';
-import './App.css';
+import "./App.css";
+import { useEffect, useState } from "react";
+import jwt_decode from "jwt-decode";
+import ReactJson from "react-json-view";
 
 function App() {
+  const [userObject, setUserObject] = useState(null);
+
+  function handleCallbackResponse(response) {
+    console.log("Encoded JWT ID Token: " + response.credential);
+    setUserObject(jwt_decode(response.credential));
+  }
+
+  useEffect(() => {
+    /* global google */
+    google.accounts.id.initialize({
+      client_id: "39848886389-eoi1f3u4eth91n7fpiosasbbn6p0spa2.apps.googleusercontent.com",
+      callback: handleCallbackResponse,
+    });
+
+    google.accounts.id.renderButton(document.getElementById("signInDiv"), { theme: "outline", size: "large" });
+  }, []);
+
+  const styles= {
+    App : {
+      display: "flex",
+      justifyContent: "center",
+      alignItems: "center",
+      height: "100vh"
+    }
+  }
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="App" style={styles.App}>
+      <div id="signInDiv"></div>
+
+      <ReactJson src={userObject || ''} />
     </div>
   );
 }
